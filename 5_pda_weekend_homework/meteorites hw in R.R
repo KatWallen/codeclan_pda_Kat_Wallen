1a8ba9d36cfb592513002cd0b92c676e99c185ea
KatWallen <108000525+KatWallen@users.noreply.github.com>
2022-07-25 10:23:13 +0100

    pda meterorite project
    session1

---
+++ 5_pda_weekend_homework/meteorites hw in R.R
@@ -0,0 +1,20 @@
+---
+  title: "Meteorites wkend H/W - PDA"
+output: html_notebook
+---
+  
+```{r}
+library(tidyverse)
+library(utils)
+```
+```{r}
+mets <- read.csv("User\\user\\Documents\\GitHub\\Code_clan_HW_KatrinaWallen\\week_02\\day_05\\pda_weekend_homework\\data\\meteorite_landings.csv")
+```
+
+```{r}
+library(data.table 
+mets <- fread("data\\meteorite_landings.csv"")
+mets
+```
+# Whatever I've tried in .R it doesn't seem to work
+
---
+++ 5_pda_weekend_homework/pda_wkend_programming/pda_wkend_prog/meteorites hw in R.R
@@ -0,0 +1,14 @@
+---
+  title: "Meteorites wkend H/W - PDA"
+output: html_notebook
+---
+  
+```{r}
+library(tidyverse)
+```
+
+```{r}
+read.csv("GitHub/Code_clan_HW_KatrinaWallen/week_02/day_05/5_pda_weekend_homework/pda_wkend_programming/pda_wkend_prog//meteorite_landings.csv")
+
+```
+
